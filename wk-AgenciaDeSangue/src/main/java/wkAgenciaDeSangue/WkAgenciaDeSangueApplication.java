7acf90bd4d4ba41abcde863ca289fa75a72242b9
ThiagoPolli <thiagopolli.palma@gmail.com>
2023-06-16 18:22:21 -0300

    initialize project

---
+++ wk-AgenciaDeSangue/src/main/java/wkAgenciaDeSangue/WkAgenciaDeSangueApplication.java
@@ -0,0 +1,13 @@
+package wkAgenciaDeSangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkAgenciaDeSangueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WkAgenciaDeSangueApplication.class, args);
+	}
+
+}
---
+++ wk-AgenciaDeSangue/src/test/java/wkAgenciaDeSangue/WkAgenciaDeSangueApplicationTests.java
@@ -0,0 +1,13 @@
+package wkAgenciaDeSangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WkAgenciaDeSangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
